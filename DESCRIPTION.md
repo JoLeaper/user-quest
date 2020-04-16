acd65797c03e0f4c11d38f6114689fd7aba0c55c
JoLeaper <joleap123@gmail.com>
2020-04-16 13:40:00 -0700

    ensured all HTML pages are linked up

---
+++ DESCRIPTION.md
@@ -0,0 +1,11 @@
+This lab will be a Pokemon Adventure!
+
+Your goal as a trainer will be to reach the Pokemon League!
+
+To do so, you will need to travel across Kanto and battle a gym in each town to get their respective badge.
+
+When you arrive to a town, you will have three types of Pokemon that you will be able to catch! If you pick the most effective one,
+you will keep your health, but picking the least effective one will net you more exp!
+
+
+
---
+++ data/gym-data.js
@@ -0,0 +1,352 @@
+const pewter = {
+    id: 'pewter',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const cerulean = {
+    id: 'cerulean',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const vermillion = {
+    id: 'vermillion',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const saffron = {
+    id: 'saffron',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const celadon = {
+    id: 'celadon',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const fuchsia = {
+    id: 'fuchsia',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const cinnabar = {
+    id: 'cinnabar',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+const viridian = {
+    id: 'viridian',
+    title: 'Showdown in Pewter City',
+    description: `
+        You arrive to Pewter City, home of the rugged Rock-Type Gym Leader Brock! To prepare for the battle ahead, you decide to
+        pick up a new Pokemon! What type of Pokemon do you add to your team?
+    `,
+    choices: [
+        {
+            id: 'grass',
+            description: 'Look for a Grass-Type on Route 2',
+            result: `
+                    You have the makings of a great trainer! Grass-Type Pokemon are super effective against Rock-Types!
+                    You let your newly acquired Grass-Type handle the Gym and come out on top, earning you the coveted Boulder Badge!
+                    `,
+            hp: 0,
+            experience: 10
+        },
+        {
+            id: 'bug',
+            description: 'Look for a Bug-Type on Route 3.',
+            result: `
+                    Someone should have studied more! Bug-Type pokemon are weak aagainst Rock-Types! However, you create a genius
+                    strategy that allows you to win despite the disadvantage and earn the Boulder Badge! 
+                    `,
+            hp: -20,
+            experience: 40
+        },
+        {
+            id: 'psychic',
+            description: 'Go look for a Psychic-Type in town.',
+            result: `
+                    You find an old man with a young Psychic-Type Pokemon. He asks you to take it so it can go on another journey,
+                    and you accept. Psychic is neutral against Rock, so the playing field is even. You beat Brock and earn the
+                    Boulder Badge!
+                    `,
+            hp: -10,
+            pokemon: 20
+        },
+    ]
+};
+
+
+
+
+
+
+/*
+Showdown in Pewter City
+The Water Flowers of Cerulean City
+Vermillion City: Electric Shock Showdown
+The Psychic Showdown: Saffron City
+Celadon City Scent-sation
+Fuchsia City Ninja Showdown
+Volcanic Panic on Cinnabar Island
+Battle of the Badge: Viridian City
+
+*/
